7fbbfe3dd4a1c0e5fd6f7ca2fdc501b946c60b42
Mikhail Stolpovskiy <mikhail.stolpovskiy@apc.univ-paris7.fr>
2016-05-17 17:49:28 +0200

    Forgot to add plot.py

---
+++ plot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as mp
+
+class Plot(object):
+    def __init__(self, name,
+                 axes):
+        self.name = name
+        self.axes = axes
+
+    def draw(self):
+        fig = mp.figure()
+        for iax, ax in enumerate(self.axes):
+            a = fig.add_subplot(iax + 1, len(self.axes), 1)
+            a.set_position(ax['position'])
+            lines = ax['lines']
+            for l in lines:
+                a.plot(l[0], l[1], c=l[2], ls=l[3], alpha=l[4],
+                       label=l[5], marker=l[6], lw=l[7], markersize=l[8])
